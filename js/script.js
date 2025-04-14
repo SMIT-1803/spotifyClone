6574c25db153fea4d7fbf41a0cfd558b7a235c37
SMIT-1803 <smitsachin1@gmail.com>
2025-04-14 14:23:48 -0700

    Fixed file location

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ async function displayAlbums(){
       const e = array[index];
     if(e.href.includes("/songs")){
       folder = e.href.split("/").slice(-2)[0];
-      let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+      let a = await fetch(`/songs/${folder}/info.json`);
       let response = await a.json();
       cardContainer.innerHTML+=`
       <div data-folder="${folder}" class="card weight400">
